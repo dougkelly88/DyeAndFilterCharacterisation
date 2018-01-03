51ce9c1c5da0c12dcdcc25b97b2d80cb36c4562c
Doug Kelly <dougkelly88@googlemail.com>
2018-01-03 13:13:03 +0000

    Step through results n at a time rather than relying on changing the url

--- semrock-scraper/scraper.py
+++ semrock-scraper/scraper.py
@@ -9,14 +9,24 @@ import scrapy
 
 class SemrockSpider(scrapy.Spider):
     name="semrock_spider"
-    max_filters = 2000
+    max_filters = 20
     #start_urls=['https://www.semrock.com/filters.aspx']
     start_urls=['https://www.semrock.com/filtersRefined.aspx?page=1&minWL=0&maxWL=2000&so=0&recs='+str(max_filters)]
     
     def parse(self, response):
         FILTER_SELECTOR='.partResultsItem'
         for filt in response.css(FILTER_SELECTOR):
-            NAME_SELECTOR = 'h1 a ::text'
+            PART_SELECTOR = 'h1 a ::text'
+            LINK_SELECTOR = 'h1 a ::attr(href)'
             yield{
-                'name': filt.css(NAME_SELECTOR).extract_first().strip(), 
+                'name': filt.css(PART_SELECTOR).extract_first().strip(), 
+                'link': filt.css(LINK_SELECTOR).extract_first().strip()
             } 
+            
+        NEXT_PAGE_SELECTOR = '.paging_link a ::attr(href)'
+        next_page = response.css(NEXT_PAGE_SELECTOR).extract()[-1]
+        if next_page:
+            yield scrapy.Request(
+                response.urljoin(next_page),
+                callback=self.parse
+            )
