6751abc2c68333599b6638a326922d16811c0e38
d.kelly <d.kelly@base4.co.uk>
2018-01-04 12:50:01 +0000

    First commit of scraper for downloading Chroma filter library, checking that ascii file link can be accessed from the filter details page

---
+++ chroma-scraper/scraper.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  3 12:18:56 2018
+
+@author: Doug
+
+Usage: from base directory, type: scrapy runspider scraper.py
+"""
+
+import scrapy
+from scrapy.http import Request
+from scrapy.xlib.pydispatch import dispatcher
+from scrapy import signals
+from scrapy.spidermiddlewares.httperror import HttpError
+import os
+
+class ChromaSpider(scrapy.Spider):
+    name="chroma_spider"
+    filters_per_page = 20
+    #start_urls=['https://www.semrock.com/filters.aspx']
+    start_urls=['https://www.chroma.com/products/custom-inventory/nc462712-t365lpxt']
+    
+#    def __init__(self, category=None):
+#        self.failed_filters = []
+#        self.pn = ''
+#        dispatcher.connect(self.handle_spider_closed, signals.spider_closed)
+#        os.mkdir('Dichroic')
+#        os.mkdir('Long pass')
+#        os.mkdir('Short pass')
+#        os.mkdir('Filter')
+        
+    
+    def parse(self, response):
+        DATA_LINK_SELECTOR = '//*[@id="tabs-detail_page_plot-left"]/div/div[1]/div/div/div/div/div[2]/div/table/tbody/tr/td[6]/span/a/@href'
+        NAME_SELECTOR = 'body > div.wrapper > div > div.layout > div.body.clearfix > div > div.content > div > div > div > div.content-top > div > div > div > div > div.layout-plot-region.layout-plot-top > h2 > a ::text'
+        yield {
+            'filt name': response.css(NAME_SELECTOR).extract_first().strip(), 
+            'lnk': response.xpath(DATA_LINK_SELECTOR).extract_first()
+            }
+            
+#        NEXT_PAGE_SELECTOR = '.paging_link a ::attr(href)'
+#        next_page = response.css(NEXT_PAGE_SELECTOR).extract()[-1]
+#        if next_page:
+#            yield scrapy.Request(
+#                response.urljoin(next_page),
+#                callback=self.parse
+#            )
+            
+#    def save_txt(self, response):
+#               
+#        path = response.url.split('/')[-1]
+#        if (path[0:2]) == 'Di':
+#            path = os.path.join('Dichroic', path)
+#        elif (path[0:2]) == 'LP':
+#            path = os.path.join('Long pass', path)
+#        elif (path[0:2] == 'SP'):
+#            path = os.path.join('Short pass', path)
+#        else:
+#            path = os.path.join('Filter', path)
+#        
+#        self.logger.info('Saving txt %s', path)
+#        with open(path, 'wb') as f:
+#            f.write(response.body)
+            
+    def handle_spider_closed(self, spider):
+        self.logger.info('FINISHED!')
+        self.logger.info('There were %d failed filters', len(self.failed_filters))
+        self.logger.info('Failed at %s', ', '.join(self.failed_filters))
+        with open('failures.csv', 'w') as f:
+            f.write('\n'.join(self.failed_filters))
+        
+    def handle_errs(self, failure):
+        # log all failures
+        self.logger.error(repr(failure))
+
+        if failure.check(HttpError):
+            # these exceptions come from HttpError spider middleware
+            # you can get the non-200 response
+            response = failure.value.response
+            self.logger.info('\n\nFAILURE!\n')
+            self.logger.info('%s\n\n', response.url.split('/')[-1].replace('_Spectrum.txt', '\n'))            
+            self.failed_filters.append(response.url.split('/')[-1].replace('_Spectrum.txt', ''))
+    
